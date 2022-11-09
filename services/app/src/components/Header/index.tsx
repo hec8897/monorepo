218aa369cc1a3d91df55d8dac3f831e89038a24e
kim dawoon <dw.kim1@greenlabs.co.kr>
2022-11-09 23:21:40 +0900

    title 컴포넌트 분리
    Signed-off-by: kim dawoon <dw.kim1@greenlabs.co.kr>

--- services/app/src/components/Header/index.tsx
+++ services/app/src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from 'react';
+import { Title } from '@common/component';
 import { BsChevronCompactLeft, BsList } from 'react-icons/bs';
 
 const Header = ({ children }: { children: ReactNode }) => (
   <header className="border-b p-2 py-3 flex justify-between items-center">
     <BsChevronCompactLeft size={24} />
-    <h1 className="text-2xl font-bold">{children}</h1>
+    <Title>{children}</Title>
     <BsList size={32} />
   </header>
 );
