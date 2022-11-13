6ad056c051ff420430b88f0a28a09e28c911562d
kim dawoon <dw.kim1@greenlabs.co.kr>
2022-11-13 21:26:22 +0900

    login 컴포넌트 적용
    Signed-off-by: kim dawoon <dw.kim1@greenlabs.co.kr>

--- services/app/src/pages/index.tsx
+++ services/app/src/pages/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Flex } from '@common/component';
+import { Flex, Title } from '@common/component';
 
 const Login = () => {
   return (
-    <div className="bg-slate-100">
-      <Flex center height="h-20">
-        <div>1</div>
-        <div>2</div>
-      </Flex>
-    </div>
+    <Flex height="h-screen" center col>
+      <Title>MONOREPO-APP</Title>
+      <div>input1</div>
+      <div>input2</div>
+      <div>button</div>
+    </Flex>
   );
 };
 
